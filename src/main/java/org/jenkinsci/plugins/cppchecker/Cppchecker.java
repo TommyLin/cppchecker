60268294110177cfd6c3dafa9696934582bad140
Tommy Lin <tommylin@socle-tech.com>
2017-07-28 08:55:57 +0800

    Invoke netbeans coding style command "Format".

--- src/main/java/org/jenkinsci/plugins/cppchecker/Cppchecker.java
+++ src/main/java/org/jenkinsci/plugins/cppchecker/Cppchecker.java
@@ -1,4 +1,5 @@
 package org.jenkinsci.plugins.cppchecker;
+
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.FilePath;
@@ -22,14 +23,13 @@ import java.io.IOException;
  *
  * <p>
  * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link Cppchecker} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
- * to remember the configuration.
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked and a new
+ * {@link Cppchecker} is created. The created instance is persisted to the
+ * project configuration XML by using XStream, so this allows you to use
+ * instance fields (like {@link #name}) to remember the configuration.
  *
  * <p>
- * When a build is performed, the {@link #perform} method will be invoked. 
+ * When a build is performed, the {@link #perform} method will be invoked.
  *
  * @author Kohsuke Kawaguchi
  */
@@ -45,6 +45,7 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
 
     /**
      * We'll use this from the {@code config.jelly}.
+     *
      * @return name
      */
     public String getName() {
@@ -52,15 +53,16 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
     }
 
     @Override
-    public void perform(Run<?,?> build, FilePath workspace, Launcher launcher, TaskListener listener) {
+    public void perform(Run<?, ?> build, FilePath workspace, Launcher launcher, TaskListener listener) {
         // This is where you 'build' the project.
         // Since this is a dummy, we just say 'hello world' and call that a build.
 
         // This also shows how you can consult the global configuration of the builder
-        if (getDescriptor().getUseFrench())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
+        if (getDescriptor().getUseFrench()) {
+            listener.getLogger().println("Bonjour, " + name + "!");
+        } else {
+            listener.getLogger().println("Hello, " + name + "!");
+        }
     }
 
     // Overridden for better type safety.
@@ -68,22 +70,24 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
     // you don't have to do this.
     @Override
     public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl)super.getDescriptor();
+        return (DescriptorImpl) super.getDescriptor();
     }
 
     /**
-     * Descriptor for {@link Cppchecker}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
+     * Descriptor for {@link Cppchecker}. Used as a singleton. The class is
+     * marked as public so that it can be accessed from views.
      *
      * <p>
-     * See {@code src/main/resources/hudson/plugins/hello_world/Cppchecker/*.jelly}
+     * See
+     * {@code src/main/resources/hudson/plugins/hello_world/Cppchecker/*.jelly}
      * for the actual HTML fragment for the configuration screen.
      */
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
         /**
-         * To persist global configuration information,
-         * simply store it in a field and call save().
+         * To persist global configuration information, simply store it in a
+         * field and call save().
          *
          * <p>
          * If you don't want fields to be persisted, use {@code transient}.
@@ -91,8 +95,8 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
         private boolean useFrench;
 
         /**
-         * In order to load the persisted global configuration, you have to 
-         * call load() in the constructor.
+         * In order to load the persisted global configuration, you have to call
+         * load() in the constructor.
          */
         public DescriptorImpl() {
             load();
@@ -101,23 +105,25 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
         /**
          * Performs on-the-fly validation of the form field 'name'.
          *
-         * @param value
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
-         *      <p>
-         *      Note that returning {@link FormValidation#error(String)} does not
-         *      prevent the form from being saved. It just means that a message
-         *      will be displayed to the user. 
+         * @param value This parameter receives the value that the user has
+         * typed.
+         * @return Indicates the outcome of the validation. This is sent to the
+         * browser.
+         * <p>
+         * Note that returning {@link FormValidation#error(String)} does not
+         * prevent the form from being saved. It just means that a message will
+         * be displayed to the user.
          * @throws java.io.IOException TODO: Add description
          * @throws javax.servlet.ServletException TODO: Add description
          */
         public FormValidation doCheckName(@QueryParameter String value)
                 throws IOException, ServletException {
-            if (value.length() == 0)
+            if (value.length() == 0) {
                 return FormValidation.error("Please set a name");
-            if (value.length() < 4)
+            }
+            if (value.length() < 4) {
                 return FormValidation.warning("Isn't the name too short?");
+            }
             return FormValidation.ok();
         }
 
@@ -129,6 +135,7 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
 
         /**
          * This human readable name is used in the configuration screen.
+         *
          * @return The name
          */
         @Override
@@ -144,14 +151,17 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
             // ^Can also use req.bindJSON(this, formData);
             //  (easier when there are many fields; need set* methods for this, like setUseFrench)
             save();
-            return super.configure(req,formData);
+            return super.configure(req, formData);
         }
 
         /**
-         * This method returns true if the global configuration says we should speak French.
+         * This method returns true if the global configuration says we should
+         * speak French.
          *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
+         * The method name is bit awkward because global.jelly calls this method
+         * to determine the initial state of the checkbox by the naming
+         * convention.
+         *
          * @return useFrench
          */
         public boolean getUseFrench() {
@@ -159,4 +169,3 @@ public class Cppchecker extends Builder implements SimpleBuildStep {
         }
     }
 }
-
